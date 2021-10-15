e13e6c6f8dee7c6012dfa2dfaa9ebf3f40f2d553
e1630m <e1630m@gmail.com>
2021-10-15 23:08:28 +0000

    Update pset1/credit

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -7,83 +7,45 @@
 int is_number(string card)
 {
     for (int i = 0; card[i] != '\0'; i++)
-    {
         if (!isdigit(card[i]))
         {
             return 0;
         }
-    }
     return 1;
 }
 
-int luhn(string card, int length)
+int luhn(string card, int last_idx)
 {
-    int sum1 = 0, sum2 = 0, i = 0;
-    while (i <= length)
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i <= last_idx; i++)
     {
         if (!(i % 2))
         {
-            sum1 += card[length - i] - '0';
+            sum1 += card[last_idx - i] - '0';
         }
         else
         {
-            int tmp = (card[length - i] - '0') * 2;
-            if (tmp >= 10)
-            {
-                sum2 += tmp % 10 + 1;
-            }
-            else
-            {
-                sum2 += tmp;
-            }
+            int tmp = (card[last_idx - i] - '0') * 2;
+            sum2 += tmp < 10 ? tmp : tmp % 10 + 1;
         }
-        i++;
     }
     return (!((sum1 + sum2) % 10));
 }
 
 int main(void)
 {
-    while (1)
+    string card = "Not a number";
+    while (!is_number(card))
     {
-        string card = get_string("Number: ");
-        int length = strlen(card);
-        int f = card[0] - '0', s = card[1] - '0';
-        if (is_number(card))
-        {
-            if (luhn(card, length - 1))
-            {
-                if (length == 15 && f == 3 && (s == 4 || s == 7))
-                {
-                    printf("AMEX\n");
-                }
-                else if (f == 4 && length == 13)
-                {
-                    printf("VISA\n");
-                }
-                else if (length == 16)
-                {
-                    if (f == 5 && 0 < s && s < 6)
-                    {
-                        printf("MASTERCARD\n");
-                    }
-                    else if (f == 4)
-                    {
-                        printf("VISA\n");
-                    }
-                    else
-                    {
-                        printf("INVALID\n");
-                    }
-                }
-                else
-                {
-                    printf("INVALID\n");
-                }
-                break;
-            }
-            printf("INVALID\n");
-            break;
-        }
+        card = get_string("Number: ");
     }
+    int len = strlen(card), f = card[0] - '0', s = card[1] - '0';
+    string brand = "INVALID\n";
+    if (luhn(card, len - 1))
+    {
+        brand = (len == 15 && f == 3 && (s == 4 || s == 7)) ? "AMEX\n" : brand;
+        brand = ((len == 13 && f == 4) || (len == 16 && f == 4)) ? "VISA\n" : brand;
+        brand = (len == 16 && f == 5 && s > 0 && s < 6) ? "MASTERCARD\n" : brand;
+    }
+    printf("%s", brand);
 }
